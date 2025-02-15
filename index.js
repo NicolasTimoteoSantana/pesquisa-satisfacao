d4b91ab3c52e99bf654b51cacd189e1ac6c04369
Nicolas Timoteo Santana <nicolassantana193@gmail.com>
2025-02-15 00:05:50 -0300

    SITE
    SITE

---
+++ index.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+  const mensagem = "Tudo que eu mais queria agora e a cada segundo que eu respiro é estar agarradinho com a minha princesinha, ouvir ela falar de qualquer coisa por horas, EU TE AMO MUITÃO";
+  document.getElementById('mensagem-especial').innerText = mensagem;}
+  
