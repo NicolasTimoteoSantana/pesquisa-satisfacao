ac845a0e2884248153b56b2a9bb626cf7b15b7f5
Nicolas Timoteo Santana <nicolassantana193@gmail.com>
2025-02-18 21:01:46 -0300

    aswea
    sf

---
+++ Pesquisa STISFAÇÃO-20250218T233515Z-001/Pesquisa STISFAÇÃO/index..js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = "Amo quando voce esta comigo. Amo quando estou com você. Amo a sua risada e o jeito que você me olha, ouvir a sua voz e sentir você comigo minha fofinha. Amo amar você.";
+    document.getElementById("botao").innerText = mensagem;}
+    
